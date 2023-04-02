bf610128b7d6bd67cf70410900f2fb43ecfd9963
Anar <anar.k14@mail.ru>
2023-04-02 14:07:57 +0400

    fibonacci numbers

---
+++ B_ControlFlow/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace B_ControlFlow;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of Fibonacci numbers you want to generate");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] fibonacci = new int[n];
+
+        int a0 = 0;
+        int a1 = 1;
+
+        fibonacci[0] = a0;
+        fibonacci[1] = a1;
+
+        for(int i = 2;i < n; i++)
+        {
+            int a = a0 + a1;
+            fibonacci[i] = a;
+
+            a0 = a1;
+            a1 = a;
+
+        }
+
+        foreach(int cur in fibonacci)
+        {
+            Console.WriteLine(cur);
+        }
+        Console.ReadLine();
+
+
+    }       
+}
+
---
+++ CSharpKurs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace CSharpKurs;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ukajite Imya");
+        string name = Console.ReadLine();
+        Console.WriteLine("ukajite vozrast");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Napiwite familiyu");
+        string userName = Console.ReadLine();
+
+
+
+
+        string total =
+            $"Spasibo Mr. :{Environment.NewLine}"
+           + $"Vawe imya: {name}{Environment.NewLine}"
+           + $"Vaws Familiya :{userName}{Environment.NewLine}"
+        +$"Vaw vozrast:{age}{Environment.NewLine}";
+        Console.WriteLine(total);
+    }
+}
+
+
+
+
+
+
